e5e11529ce6f579c728ad364b44275a8f9560b60
Sendhil Kumar R <sendhil.r@gmail.com>
2017-09-14 11:47:03 +0530

    Added a ramda cond version of the reducer

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { cond, equals, always, T } from 'ramda';
 import applyState from './hoc-wrappers/with-state.jsx';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case '@@INIT': return { count: action.payload.props.initialCount };
-    case 'INC': return { count: state.count + 1 };
-    case 'DEC': return { count: state.count - 1 };
-    default:
-      break;
-  }
-  return state;
-};
+const reducer = (state, action) => cond([
+  [equals('@@INIT'), () => ({ count: action.payload.props.initialCount })],
+  [equals('INC'), () => ({ count: state.count + 1 })],
+  [equals('DEC'), () => ({ count: state.count - 1 })],
+  [T, always(state)],
+])(action.type);
 
 const Counter = ({ count, localDispatch }) => (<div>
   <button onClick={localDispatch('DEC')}>-</button>
   Counter: {count}
-  <button onClick={localDispatch('INC')}>+</button>
+  <button onClick={localDispatch('INC1')}>+</button>
 </div>);
 
 Counter.propTypes = {
